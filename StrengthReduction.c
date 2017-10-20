78005b6db2ce2e6d1813498ad05d0ba37156d947
aSempruch <32146438+aSempruch@users.noreply.github.com>
2017-10-20 00:06:41 -0400

    Updated strength reduction

--- StrengthReduction.c
+++ StrengthReduction.c
@@ -14,6 +14,15 @@
 #include "InstrUtils.h"
 #include "Utils.h"
 
+int logb2(double num){
+    int result = 0;
+    while((int)num > 1){
+        num = num/2;
+        result++;
+    }
+    return result;
+}
+
 void Optimize(Instruction *i1, Instruction *i2, Instruction *i3){
     
     if(i1->opcode == LOADI){ //Found LOADI operation
@@ -21,14 +30,14 @@ void Optimize(Instruction *i1, Instruction *i2, Instruction *i3){
         
         if(val && (!(val&(val-1)))){ //val is a power of 2
             if(i2->opcode == DIV){ //DIV detected - performing optimization
-                int log = (int)log((double)i1->field1);
+                int log = logb2((double)i1->field1);
                 i1->opcode = RSHIFTI;
                 i1->field1 = i2->field1;
                 i1->field2 = log;
                 i1->field3 = i2->field3;
             }
             else if(i2->opcode == MUL) { //MUL detected - performing optimization
-                int log = (int)log((double)i1->field1);
+                int log = logb2((double)i1->field1);
                 i1->opcode = LSHIFTI;
                 i1->field1 = i2->field1;
                 i1->field2 = log;
