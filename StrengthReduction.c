1cd1365b42917604eab896fad435a4f61160afb6
aSempruch <32146438+aSempruch@users.noreply.github.com>
2017-10-19 21:46:17 -0400

    Added strength reduction

--- StrengthReduction.c
+++ StrengthReduction.c
@@ -14,6 +14,34 @@
 #include "InstrUtils.h"
 #include "Utils.h"
 
+void Optimize(Instruction *i1, Instruction *i2, Instruction *i3){
+    
+    if(i1->opcode == LOADI){ //Found LOADI operation
+        int val = i1->field1;
+        
+        if(val && (!(val&(val-1)))){ //val is a power of 2
+            if(i2->opcode == DIV){ //DIV detected - performing optimization
+                int log = (int)log((double)i1->field1);
+                i1->opcode = RSHIFTI;
+                i1->field1 = i2->field1;
+                i1->field2 = log;
+                i1->field3 = i2->field3;
+            }
+            else if(i2->opcode == MUL) { //MUL detected - performing optimization
+                int log = (int)log((double)i1->field1);
+                i1->opcode = LSHIFTI;
+                i1->field1 = i2->field1;
+                i1->field2 = log;
+                i1->field3 = i2->field3;
+            }
+            free(i2);
+            i1->next = i3;
+            if(i3 != NULL)
+                i3->prev = i1;
+        }
+    }
+}
+
 int main()
 {
 	Instruction *head;
@@ -24,7 +52,12 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	/* YOUR CODE GOES HERE */
+	Instruction *ptr = head;
+        
+        while(ptr->next != NULL){
+            Optimize(ptr, ptr->next, ptr->next->next);
+            ptr = ptr->next;
+        }
 
 	if (head) 
 		PrintInstructionList(stdout, head);
