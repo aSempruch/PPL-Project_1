1e2658d33ced15b928e0399494a7b8072a074252
aSempruch <32146438+aSempruch@users.noreply.github.com>
2017-10-20 21:01:58 -0400

    Added Constant Folding

--- ConstFolding.c
+++ ConstFolding.c
@@ -13,6 +13,34 @@
 #include "InstrUtils.h"
 #include "Utils.h"
 
+void Optimize(Instruction *i1, Instruction *i2, Instruction *i3){
+    if(i1->opcode == LOADI && i2->opcode == LOADI){ //Found two consecutive LOADI instructions
+        
+        if(i3->opcode == ADD || i3->opcode == SUB || i3->opcode == MUL){ //Found OP
+            int a = i1->field1;
+            int b = i2->field1;
+            int c;
+            
+            if(i3->opcode == ADD)
+                c = a + b;
+            else if(i3->opcode == SUB)
+                c = a - b;
+            else
+                c = a * b;
+                        
+            i1->field1 = c;
+            i1->field2 = i3->field3;
+            i1->next = i3->next;
+            
+            free(i2);
+            free(i3);
+            
+            if(i1->next != NULL)
+                i1->next->prev = i1;
+        }
+    }
+}
+
 int main()
 {
 	Instruction *head;
@@ -23,7 +51,11 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	/* YOUR CODE GOES HERE */
+	Instruction *ptr = head;
+        while(ptr->next->next != NULL){
+            Optimize(ptr, ptr->next, ptr->next->next);
+            ptr = ptr->next;
+        }
 
 	if (head) 
 		PrintInstructionList(stdout, head);
